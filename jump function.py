9db55fff33d357b171803ee3052e0150312d74e1
josheldiablo <72643648+josheldiablo@users.noreply.github.com>
2021-05-11 13:57:49 +0530

    Add files via upload

---
+++ jump function.py
@@ -0,0 +1,57 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((1000, 700))
+
+pygame.display.set_caption("MAnmnanmns")
+
+x = 50
+y = 600
+width = 40
+height = 60
+vel = 7
+
+isJump = False
+jumpCount = 10
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x >= vel:
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x <= 1000 - width - vel:
+        x += vel
+
+    if not (isJump):
+        if keys[pygame.K_UP] and y >= vel:
+            y -= vel
+
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+
+        else:
+            isJump = False
+            jumpCount = 10
+        
+
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
+    
+
+pygame.quit()
